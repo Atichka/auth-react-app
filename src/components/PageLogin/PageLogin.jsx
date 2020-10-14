f54370cba1f72ee98a3b7183163b1a199a8c755f
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-10-14 23:51:55 +0300

    fix api

--- src/components/PageLogin/PageLogin.jsx
+++ src/components/PageLogin/PageLogin.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, getUsers } from 'react';
 import s from './PageLogin.module.css';
+import { useHistory } from 'react-router-dom';
 
 const PageLogin = () => {
-    return <form className={s.auth}> 
-            <input name="login" type="text" validations={{minLength: 5}} validationErrors={{minLength: 'Введите минимум 5 символов'}} required/>
-            <input type='password' name='password' placeholder='пароль' validations={{minLength: 5}} validationErrors={{minLength: 'Введите минимум 5 символов'}} required/>
-            <button>Вход</button>
-        </form>
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const submit = () => {
+    fetch("http://emphasoft-test-assignment.herokuapp.com/swagger/api/v1/api-token-auth/" , {
+      method: 'POST',
+      body: JSON.stringify({
+        login: login,
+        password: password
+      })
+    })
+    // .then((res) => {
+    //   if (res.ok) {
+    //     return useHistory.push({pathname: '/users'}));
+    //   }
+    //   return Promise.reject(`Что-то пошло не так: ${res.status}`);
+    // })
+        .then(res => res.json())
+        .then(() => history.push({pathname: '/users'}))
+        .catch((res) => Promise.reject(`Что-то пошло не так: ${res.status}`))
+  };
+
+  return <form className={s.auth} onSubmit={submit}>
+      <input
+        value={login}
+        onChange={event => setLogin(event.target.value)}
+        name="login"
+        type="text"
+        placeholder='логин' required/>
+      <input
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+        name='password'
+        type='password'
+        placeholder='пароль' required/>
+      <button onClick={submit} type="submit">Вход</button>
+    </form>
 }
 
 export default PageLogin;
